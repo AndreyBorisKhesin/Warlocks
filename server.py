3128462e3d8a3e302f365ca5113a76e53e6920b6
Andrey Boris Khesin <andrey.khesin@gmail.com>
2018-01-20 13:54:51 -0500

    server works

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods = ['POST'])
 def root():
 	#do stuff with request.header style variables
-	return "Borders Within Doctors"
+	return 'Borders Within Do2'
 
-@app.route('/test1', methods=['POST'])
-def test1():
-	return "Response for test1!"
+@app.route('/test', methods = ['POST'])
+def test():
+	return 'test'
