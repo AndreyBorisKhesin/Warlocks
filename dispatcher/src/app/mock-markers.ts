edfc71424fe45a1f2e809f0f02e2827d64f4ce57
candalistic <candalistic@gmail.com>
2018-01-20 13:22:40 +0300

    Reorganized and modularized map code according to Angular tutorial. Map is now defined in a separate component, markers data is provided via map service.

---
+++ dispatcher/src/app/mock-markers.ts
@@ -0,0 +1,19 @@
+import { Marker } from './map/map.component';
+
+export const MARKERS: Marker[] = [
+    {
+        lat: 43.6595053,
+        lng: -79.3978192,
+        label: 'M1'
+    },
+    {
+        lat: 43.6795053,
+        lng: -79.3778192,
+        label: 'M2'
+    },
+    {
+        lat: 51.723858,
+        lng: 7.895982,
+        label: 'M3'
+    }
+]
