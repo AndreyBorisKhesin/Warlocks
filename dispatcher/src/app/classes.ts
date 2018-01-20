8b5dd7b9640e0d611536006d4dbbd8ed6bbd9367
AsicChen <a19c97@hotmail.com>
2018-01-20 13:40:47 -0500

    update

--- dispatcher/src/app/classes.ts
+++ dispatcher/src/app/classes.ts
@@ -11,13 +11,18 @@ export class Emergency {
     sex: string;
     age: number;
     location: string;
+    lat: number;
+    long: number;
     symptoms: string;
 
-    constructor(_name: string, _sex: string, _age: number, _location: string, _symptoms: string){
+    constructor(_name: string, _sex: string, _age: number, _location: string,
+      _lat: number, _long: number, _symptoms: string){
         this.name = _name;
         this.sex = _sex;
         this.age = _age;
         this.location = _location;
+        this.lat = _lat;
+        this.long = _long;
         this.symptoms = _symptoms;
     }
 }
--- dispatcher/src/app/map/map.component.ts
+++ dispatcher/src/app/map/map.component.ts
@@ -6,7 +6,8 @@ import { Emergency } from '../classes';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
+  providers: [MapService]
 })
 export class MapComponent implements OnInit {
     title: string = 'Doctors within Borders';
@@ -14,19 +15,26 @@ export class MapComponent implements OnInit {
     lng: number = -79.3978192;
     zoom: number = 13;
     responders: Responder[];
+    haveEmergency: boolean;
 
     constructor(private mapService: MapService) { }
 
     ngOnInit() {
         this.responders = this.mapService.getResponders();
-    }
-
-    dispatch(responderId: string) {
-        // alert("Responder #" + responderId + " dispatched!");
+        this.haveEmergency = true;
+        if (this.haveEmergency) {
+            this.mapStartEmergency(this.getEmergency());
+        }
     }
 
     getEmergency(): Emergency {
-        let em = new Emergency("Andrey Khesin", "male", 19, "40 St George St, Toronto, ON M5S 2E4", "missing eyeball");
+        let em = new Emergency("Andrey Khesin", "male", 19,
+        "40 St George St, Toronto, ON M5S 2E4", 43.6596426, -79.3976676,
+        "missing eyeball");
         return em;
+    }
+
+    mapStartEmergency(em: Emergency): void {
+
     }
 }
