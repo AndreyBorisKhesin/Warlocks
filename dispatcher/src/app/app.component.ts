1235587d134d20cbf8363bd304664591944d23ea
candalistic <candalistic@gmail.com>
2018-01-20 14:09:10 +0300

    Fixed merge

--- dispatcher/src/app/app.component.ts
+++ dispatcher/src/app/app.component.ts
@@ -10,32 +10,11 @@ export class AppComponent {
   title: string = 'Doctors within Borders';
   lat: number = 43.6595053;
   lng: number = -79.3978192;
+
   zoom: number = 13;
   sex = ['Female', 'Male'];
   submitted = false;
   onSubmit() {
     this.submitted = true;
   }
-
-  markers: marker[] = [
-    {
-      lat: 43.6595053,
-      lng: -79.3978192,
-      label: 'M1'
-    }, {
-      lat: 43.6795053,
-      lng: -79.3778192,
-      label: 'M2'
-    }, {
-      lat: 51.723858,
-      lng: 7.895982,
-      label: 'M3'
-    }
-  ]
-}
-
-interface marker {
-  lat: number;
-  lng: number;
-  label?: string;
 }
