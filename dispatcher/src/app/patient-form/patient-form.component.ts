6f8e6fe954135b11df1ec6a570cc529ea6019a4a
candalistic <candalistic@gmail.com>
2018-01-21 11:03:32 +0300

    Updated emergency location to BA

--- dispatcher/src/app/patient-form/patient-form.component.ts
+++ dispatcher/src/app/patient-form/patient-form.component.ts
@@ -12,7 +12,7 @@ import { MapComponent } from '../map/map.component';
 export class PatientFormComponent implements OnInit {
   sex = ['Female', 'Male'];
 
-  model = new Emergency('', '', '', 43.657081, -79.3961597, '', 1);
+  model = new Emergency('', '', '', 43.6591039, -79.3990747, '', 1);
 
   public constructor(
     private shareFormService: ShareFormService,
